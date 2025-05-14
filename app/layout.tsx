e40eae908ed9bc77618b4ff2ce28f2e1def825bf
Michael Ehmke <michael@ehmke.ai>
2025-05-13 22:22:21 -0500

    feat: overscroll

--- app/layout.tsx
+++ app/layout.tsx
@@ -46,9 +46,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="bg-black">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-black`}
       >
         {children}
       </body>
